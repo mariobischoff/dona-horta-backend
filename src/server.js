14fa28072ea261a31e62ec72df8364986ebc0914
Thalles Sobral <thazsobral@gmail.com>
2019-11-15 06:29:14 -0300

    alteração de porta

--- src/server.js
+++ src/server.js
@@ -1,4 +1,4 @@
 import app from './app'
 require('dotenv').config()
 
-app.listen(9090, () => console.log(`server up at http://localhost:${process.env.API_PORT}`))
+app.listen(process.env.API_PORT, () => console.log(`server up at http://localhost:${process.env.API_PORT}`))
